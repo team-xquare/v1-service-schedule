3dfd46fb4cb9d977480727481a651bf4ca4686a2
smoothbear <kjbin0420@naver.com>
2022-01-01 18:46:27 +0900

    :gear: :: (#1) 기본적인 프로젝트 구조 추가

---
+++ src/main/kotlin/app/xqaure/schedule/ScheduleApplication.kt
@@ -0,0 +1,11 @@
+package app.xqaure.schedule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScheduleApplication>(*args)
+}
---
+++ src/test/kotlin/app/xqaure/schedule/ScheduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.xqaure.schedule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
