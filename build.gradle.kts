7b030f7ac251cb5fb0cccb815b9f2ece67f07f27
smoothbear <kjbin0420@naver.com>
2022-01-19 12:22:23 +0900

    setting: ⚙️ ktlint gradle plugin 의존성 추가 및 linting, checking task 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// For formatting
+val ktlint by configurations.creating
+
 plugins {
-	id("org.springframework.boot") version "2.6.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.6.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+}
+
+dependencies {
+    ktlint("com.pinterest:ktlint:0.43.2") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
 }
 
 group = "app.xqaure"
@@ -12,35 +23,61 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// Spring Boot
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
-	// Spring Cloud AWS
-	implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging")
-	implementation("org.springframework.cloud:spring-cloud-starter-aws-autoconfigure")
+    // Spring Cloud AWS
+    implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging")
+    implementation("org.springframework.cloud:spring-cloud-starter-aws-autoconfigure")
 
-	// Jackson Modules
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Jackson Modules
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// Kotlin Standard Library
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Kotlin Standard Library
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	/* Test Libraries */
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    /* Test Libraries */
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+// Checking lint
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
+
+// Formatting all source files
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
+    jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
 }
