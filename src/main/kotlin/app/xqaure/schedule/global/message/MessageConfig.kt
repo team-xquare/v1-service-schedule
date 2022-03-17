f5e26378db501795c4bacf7ca4cb274be77920f0
KIM JUNG BIN <kjbin0420@naver.com>
2022-03-17 23:16:42 +0900

    feat: add bean validation messagesource importing

---
+++ src/main/kotlin/app/xqaure/schedule/global/message/MessageConfig.kt
@@ -0,0 +1,27 @@
+package app.xqaure.schedule.global.message
+
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+
+
+@Configuration
+class MessageConfig {
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+        messageSource.setBasenames("classpath:i18n/messages")
+        messageSource.setDefaultEncoding("UTF-8")
+        return messageSource
+    }
+
+    @Bean
+    fun validatorFactoryBean(messageSource: MessageSource): LocalValidatorFactoryBean {
+        val localValidatorFactoryBean = LocalValidatorFactoryBean()
+        localValidatorFactoryBean.setValidationMessageSource(messageSource)
+        return localValidatorFactoryBean
+    }
+}
