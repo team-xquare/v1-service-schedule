69cd44aa0aafa5ce08e0ca2a3ec5bd6abee3cdd1
KIM JUNG BIN <kjbin0420@naver.com>
2022-03-17 23:18:42 +0900

    feat: response util 추가
    messagesource로부터 대신 메세지를 가져오는 역할을 수행합니다.

---
+++ src/main/kotlin/app/xqaure/schedule/presentation/dto/Response.kt
@@ -0,0 +1,26 @@
+package app.xqaure.schedule.presentation.dto
+
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class ResponseCreator(
+    private val messageSource: MessageSource
+) {
+    fun fromMessageSource(
+        code: String,
+        propertyName: String,
+        vararg args: Any?
+    ): BasicResponse {
+        return BasicResponse(
+            code = code,
+            message = messageSource.getMessage(propertyName, args, LocaleContextHolder.getLocale())
+        )
+    }
+}
+
+data class BasicResponse(
+    val code: String,
+    val message: String
+)
