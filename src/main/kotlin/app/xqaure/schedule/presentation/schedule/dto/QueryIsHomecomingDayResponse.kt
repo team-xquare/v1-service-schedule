740060b485bdb6c2d1bc8a63be98739a0a58d2f9
seongwon <seongwon1290@dsm.hs.kr>
2023-04-19 09:20:00 +0900

    ♻️ :: JsonFormat 추가

--- src/main/kotlin/app/xqaure/schedule/presentation/schedule/dto/QueryIsHomecomingDayResponse.kt
+++ src/main/kotlin/app/xqaure/schedule/presentation/schedule/dto/QueryIsHomecomingDayResponse.kt
@@ -1,5 +1,9 @@
 package app.xqaure.schedule.presentation.schedule.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class QueryIsHomecomingDayResponse(
+
+    @JsonProperty("isHomecomingDay")
     val isHomecomingDay: Boolean
 )
